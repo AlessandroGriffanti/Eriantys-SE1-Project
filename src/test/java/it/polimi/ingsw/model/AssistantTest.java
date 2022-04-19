feb2e12a8799e420c0e0aa091bcc09d6873ff9eb
tommi <tommaso.ferrario@mail.polimi.it>
2022-04-19 16:05:45 +0200

    Characters v3 + test

---
+++ src/test/java/it/polimi/ingsw/model/AssistantTest.java
@@ -0,0 +1,20 @@
+package it.polimi.ingsw.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AssistantTest {
+
+    @Test
+    void getPlayOrderValue() {
+        Assistant x = new Assistant(2,3);
+        assertEquals(2, x.getPlayOrderValue() );
+    }
+
+    @Test
+    void getMotherNatureMovementValue() {
+        Assistant x = new Assistant(2,3);
+        assertEquals(3, x.getMotherNatureMovementValue() );
+    }
+}
