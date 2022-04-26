160066ad3e06c00aca80fbfb8f2187851f29be18
Luca1056 <luca15.masiero@gmail.com>
2022-04-26 16:33:07 +0200

    Adds: - attribute masters on the class Archipelago, useful to move tower from and to the archipelagos -JavDoc comments on CloudTile class and Archipelago class

--- src/main/java/it/polimi/ingsw/model/Archipelago.java
+++ src/main/java/it/polimi/ingsw/model/Archipelago.java
@@ -2,17 +2,42 @@ package it.polimi.ingsw.model;
 
 import java.util.HashMap;
 
+/**
+ * This Class represents one group of islands-tile (at the beginning it consists of one single island-tile)
+ */
 public class Archipelago {
+    /**
+     * This attribute is the identifier of the archipelago
+     */
     private int archipelagoID;
-    private int numberOfIslands; //dobbiamo sapere quante isole fanno parte di un agglomerato
+    /**
+     * This attribute is the number of single island-tiles that belong to the archipelago
+     */
+    private int numberOfIslands;
+    /**
+     * This attribute is the number of no-entry-tiles currently on the archipelago
+     */
     private int noEntryTiles;
-    private Tower towerColor; //the towers' color is the same for every island-tile belonging to the archipelago
+    /**
+     * This attribute is the color of the Towers currently on the archipelago, or 'null' if there is no
+     * tower built on it
+     */
+    private Tower towerColor;
+    /**
+     * This attribute is the player that currently has the higher influence
+     * over the archipelago
+     */
+    private Player masterOfArchipelago; //TODO: add to UML
+    /**
+     * This attribute is the population of students of each kind currently on the island
+     */
     private HashMap<Creature, Integer> studentsPopulation;
 
     public Archipelago(int ID){
         this.archipelagoID = ID;
         this.numberOfIslands = 1;
         this.noEntryTiles = 0;
+        this.masterOfArchipelago = null;
 
         studentsPopulation = new HashMap<Creature, Integer>();
         studentsPopulation.put(Creature.DRAGON, 0);
@@ -38,6 +63,19 @@ public class Archipelago {
         return noEntryTiles;
     }
 
+    /**
+     * Compute the number of students of a particular type on the archipelago
+     * @param c type of student
+     * @return number of students of the specified type
+     */
+    public int getStudentsOfType(Creature c){
+        return studentsPopulation.get(c);
+    }
+
+    /**
+     * Computes the total number of students on the archipelago
+     * @return total number of students
+     */
     public int getTotalNumberOfStudents(){
         int sum = 0;
 
@@ -47,38 +85,78 @@ public class Archipelago {
         return sum;
     }
 
-    public int getStudentsOfType(Creature c){
-        return studentsPopulation.get(c);
-    }
-
+    /**
+     * Add one single student on the archipelago
+     * @param c
+     */
     public void addStudent(Creature c){
-        int previouseValue = studentsPopulation.get(c);
-        studentsPopulation.put(c, previouseValue + 1);
+        int previousValue = studentsPopulation.get(c);
+        studentsPopulation.put(c, previousValue + 1);
     }
 
+    /**
+     * Add more than one student, but all of them are the same type
+     * @param c type of students
+     * @param quantityToAdd number of students to put on the island
+     */
     public void addStudents(Creature c, int quantityToAdd){
-        int previouseValue = studentsPopulation.get(c);
-        studentsPopulation.put(c, previouseValue + quantityToAdd);
+        int previousValue = studentsPopulation.get(c);
+        studentsPopulation.put(c, previousValue + quantityToAdd);
     }
 
+    /**
+     * Adds one or more islands to the archipelago, and increases "numberOfIslands" accordingly
+     * @param islandsToAdd number of islands to add to the archipelago
+     */
     public void addIslands(int islandsToAdd){
         numberOfIslands += islandsToAdd;
     }
 
+    /**
+     * Adds only one no-entry-tile to the archipelago, and increases noEntryTiles by 1
+     */
     public void addNoEntryTile(){
         noEntryTiles ++;
     }
 
+    /**
+     * Adds more than one no-entry-tile to the archipelago and increases noEntryTile accordingly
+     * @param quantityToAdd number of no-entry-tile to add
+     */
     public void addNoEntryTile(int quantityToAdd){
         noEntryTiles += quantityToAdd;
     }
 
+    /**
+     * Removes one single no-entry-tile from the archipelago and decreases noEntryTile by 1
+     */
     public void removeNoEntryTile(){
         noEntryTiles --;
     }
 
-    public void buildTowers(Tower color){
-        //Lascerei al controller il compito di controllare se ci sono giá torri di altri giocatori costruite su un determinato arcipelago
-        towerColor = color;
+    /**
+     * Controls if the master is the same or has changed, in this last case retrieves towers from the new
+     * master and returns the same number of towers to the old master
+     * @param p reference to the Player for the highest influence over the archipelago
+     */
+    public void setMasterOfArchipelago(Player p){
+        if(masterOfArchipelago == null){
+            masterOfArchipelago = p;
+            masterOfArchipelago.getSchoolBoard().takeTowers(this.numberOfIslands);
+        }else if (!masterOfArchipelago.equals(p)){
+            masterOfArchipelago.getSchoolBoard().putTowers(this.numberOfIslands);
+            masterOfArchipelago = p;
+            masterOfArchipelago.getSchoolBoard().takeTowers(this.numberOfIslands);
+        }
+
+        setTowerColor();
     }
+
+    /**
+     * Sets the color of the towers given the player that is the master of the Archipelago
+     */
+    private void setTowerColor(){
+        towerColor = masterOfArchipelago.getPlayerTowerColor();
+    }
+
 }
--- src/main/java/it/polimi/ingsw/model/CloudTile.java
+++ src/main/java/it/polimi/ingsw/model/CloudTile.java
@@ -2,12 +2,34 @@ package it.polimi.ingsw.model;
 
 import java.util.ArrayList;
 
+/**
+ *This Class represents one single cloud-tile
+ */
 public class CloudTile {
+    /**
+     * This attribute is the identifier of the cloud, which is unique
+     */
     private int ID;
+    /**
+     * This attribute is the number of students that can be on the cloud at the same time
+     */
     private int capacity;
+    /**
+     * This attribute is the list of students currently on the cloud
+     */
     private ArrayList<Creature> students;
+    /**
+     * This attribute is the reference to the bag used in the match, it can be called to draw the
+     * needed students
+     */
     private Bag bag;
 
+    /**
+     * Constructor
+     * @param ID identifier of the cloud
+     * @param numOfPlayers number of players playing the match
+     * @param bag reference to the bag used in the match
+     */
     public CloudTile(int ID, int numOfPlayers, Bag bag){
         this.ID = ID;
 
@@ -20,18 +42,26 @@ public class CloudTile {
         this.students = bag.drawStudents(capacity);
     }
 
+    /**
+     * Copies the "students" attribute into a new ArrayList and replace old students with new ones, drawn
+     * from the "bag"
+     * @return ArrayList of all students found on the cloud
+     */
     public ArrayList<Creature> takeStudents(){
-        ArrayList<Creature> takenStudents = new ArrayList<Creature>();
 
-        takenStudents.addAll(this.students);
+        ArrayList<Creature> takenStudents = new ArrayList<Creature>(this.students);
         this.students.clear();
 
-    //put new students on the cloud
+        //put new students on the cloud
         putStudents(bag.drawStudents(capacity));
 
         return takenStudents;
     }
 
+    /**
+     * Puts new students on the cloud; it's private because it's only called by the methode "takeStudents"
+     * @param newStudents ArrayList of new students to put on the cloud
+     */
     private void putStudents(ArrayList<Creature> newStudents) {
         this.students = newStudents;
     }
