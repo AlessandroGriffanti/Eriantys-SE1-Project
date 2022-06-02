2420e6349df93a8009aada2754ddea0cfaad8ea5
aless <alessandro.griffanti@mail.polimi.it>
2022-06-02 23:12:52 +0200

    Added the initial situation of the islands both in NetworkHandler (handled in the initial modelView setup) and in the cli with the showStudentsOnIslands method.

--- src/main/java/it/polimi/ingsw/view/cli/ArchipelagoView.java
+++ src/main/java/it/polimi/ingsw/view/cli/ArchipelagoView.java
@@ -1,6 +1,9 @@
 package it.polimi.ingsw.view.cli;
 
 import it.polimi.ingsw.model.Archipelago;
+import it.polimi.ingsw.model.Creature;
+
+import java.util.HashMap;
 
 public class ArchipelagoView {
     /**
@@ -18,6 +21,11 @@ public class ArchipelagoView {
     private boolean motherNaturePresence; //false se non c'è, true se c'è
 
     /**
+     * This attribute is the population of students of each kind currently on the island.
+     */
+    private HashMap<Creature, Integer> studentsPopulation;
+
+    /**
      * This constructor creates a new archipelago (as a single island).
      * @param archipelagoID is the ID of the archipelago.
      */
@@ -25,8 +33,45 @@ public class ArchipelagoView {
         this.archipelagoID = archipelagoID;
         this.numberOfIsland = 1;
         this.motherNaturePresence = false;
+
+        this.studentsPopulation = new HashMap<Creature, Integer>();
+        this.studentsPopulation.put(Creature.DRAGON, 0);
+        this.studentsPopulation.put(Creature.FAIRY, 0);
+        this.studentsPopulation.put(Creature.FROG, 0);
+        this.studentsPopulation.put(Creature.UNICORN, 0);
+        this.studentsPopulation.put(Creature.GNOME, 0);
     }
 
+    /**
+     * Add one single student on the archipelago.
+     * @param c type of student.
+     */
+    public void addStudent(Creature c){
+        int previousValue = studentsPopulation.get(c);
+        studentsPopulation.put(c, previousValue + 1);
+    }
+
+    /**
+     * Compute the number of students of a particular type on the archipelago
+     * @param c type of student
+     * @return number of students of the specified type
+     */
+    public int getStudentsOfType(Creature c){
+        return studentsPopulation.get(c);
+    }
+
+    /**
+     * Computes the total number of students on the archipelago
+     * @return total number of students
+     */
+    public int getTotalNumberOfStudents(){
+        int sum = 0;
+
+        for(Creature c: Creature.values()){
+            sum += studentsPopulation.get(c);
+        }
+        return sum;
+    }
 
     public synchronized int getArchipelagoID() {
         return archipelagoID;
